b7971bc2768a539026719233ac426f12094b080c
Yamboist <rwg.amos@gmail.com>
2016-02-03 03:16:05 +0800

    Commit 1.0.0 todoapp-node beta

---
+++ public/core.js
@@ -0,0 +1,224 @@
+var objTodo = angular.module('objTodo', []);
+
+function countTodoType(todo_type, json_object){
+	var cnt = 0;
+	for(todo in json_object){
+		if(json_object[todo].todo_type == todo_type){
+			cnt++;
+		}
+	}
+	return cnt;
+}
+
+// follows YYYY-MM-DD pattern
+function parseDate(input){
+	if(input=='' || input==null) return input;
+	var dateRegex = /([0-9]+-[0-9]{2}-[0-9]{2})/
+	displayDate = dateRegex.exec(input)[1];
+	return displayDate;
+}
+
+objTodo.filter('searchFilterer', function(){
+    return function(input, search){
+        if(search == null || search == "")
+            return input;
+            
+        var out = [];
+        for(var entry in input){
+            if(input[entry].title.indexOf(search) == 0){
+                out.push(input[entry]);
+            }
+        }  
+        return out;
+    }
+});
+
+objTodo.filter('typeFilterer', function(){
+    return function(input,typeFilterx){
+        if(typeFilterx == null || typeFilterx == "")
+            return input;
+            
+        var out = [];
+        for(var entry in input){
+            if(input[entry].todo_type == typeFilterx){
+                out.push(input[entry]);
+            }
+        }  
+        return out;
+    } 
+});
+
+objTodo.filter('checkFilterer', function(){
+	return function(input, checkFilterx){
+		if(!checkFilterx) return input;
+		var out = [];
+        for(var entry in input){
+            if(checkFilterx && input[entry].progress == "100"){
+                out.push(input[entry]);
+            }
+        }  
+        return out;
+	}
+});
+
+objTodo.filter('dateFilterer',function(){
+	return function(input, date_from, date_to){
+		var out = [];
+		if(date_from != "" && date_to == ""){
+			for(var entry in input){
+				var entry_date = new Date(parseDate(input[entry].date_started));
+				var target_date = new Date(date_from);
+
+	            if(entry_date >= target_date){
+	                out.push(input[entry]);
+	            }
+        	}
+		}
+		else if(date_to != "" && date_from == ""){
+			for(var entry in input){
+				var entry_date = new Date(parseDate(input[entry].date_started));
+				var target_date = new Date(date_to);
+
+	            if(entry_date <= target_date){
+	                out.push(input[entry]);
+	            }
+        	}
+		}
+		else if(date_to != "" && date_from != ""){
+			for(var entry in input){
+				var entry_date = new Date(parseDate(input[entry].date_started));
+				var target_date_from = new Date(date_from);
+				var target_date_to = new Date(date_to);
+
+	            if(entry_date >=target_date_from && entry_date <= target_date_to){
+	                out.push(input[entry]);
+	            }
+        	}
+		}else{
+			out = input;
+		}
+
+		return out;
+	}
+});
+
+objTodo.filter('dateFilter', function(){
+	return function(input){
+		return parseDate(input);
+	}
+});
+
+function mainController($scope, $http){
+	$scope.formData = {};
+	$scope.updateData = {};
+	$scope.filters = {
+		search:'',
+		date_from:'',
+		date_to:''
+	};
+	$scope.completeChecked = false;
+    $scope.typeFilter= '';
+    $scope.searchFilter = '';
+
+	$http.get('/api/todos')
+		.success(function(data){
+			$scope.todos = data;
+			$scope.entries = $scope.todos;
+			$scope.recount();
+			console.log(data);
+		})
+		.error(function(data){
+			console.log('Error: ' + data);
+		});
+
+	$scope.recount = function(){
+		$scope.all_count = $scope.todos.length;
+		$scope.immediate_count = countTodoType("immediate", $scope.todos);
+		$scope.short_term_count = countTodoType("short-term", $scope.todos);
+		$scope.long_term_count = countTodoType("long-term", $scope.todos);
+	};
+
+	$scope.createTodo = function() {
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data){
+				$scope.formData = {};
+				$scope.todos = data;
+				$scope.recount();
+				console.log(data);
+			})
+			.error(function(data){
+				console.log('Error: '+ data);
+			});
+	};
+
+	$scope.updateTodo = function(obj,id){
+		var rootEntry = obj.target.parentNode.parentNode.parentNode;
+		var formData = new Object();
+		formData.title= rootEntry.getElementsByClassName("entry-title")[0].value;
+		formData.progress = rootEntry.getElementsByClassName("entry-progress-track")[0].value.replace("%","");
+		formData.message = rootEntry.getElementsByClassName("entry-message")[0].value;
+		formData._id = id;
+		formData.date_finished = rootEntry.getElementsByClassName("entry-date_finished")[0].value;
+		$http.put('/api/todos/update/' + id, formData )
+			.success(function(data){
+				console.log(data);
+			})
+			.error(function(data){
+				console.log('Error: '+data);
+			});
+	}
+	$scope.deleteTodo = function(){
+
+		var selectedList = document.getElementsByClassName("entry-selected");
+		if(selectedList.length == 1){
+			var _id = selectedList[0].getAttribute("data");
+			$http.delete('/api/todos/' + _id)
+				.success(function(data){
+					$scope.todos = data;
+					console.log(data);
+				})
+				.error(function(data){
+					console.log('Error: '+ data);
+				});
+		}
+		else if(selectedList.length > 1){
+			var _ids = new Array();
+			for(var i =0; i< selectedList.length; i++){
+				_ids.push(selectedList[i].getAttribute("data"));
+			}
+			var conta = new Object();
+			conta._ids = _ids;
+			$http.put('/api/todos/delete/multiple', conta)
+				.success(function(data){
+						$scope.todos = data;
+						console.log(data);
+					})
+					.error(function(data){
+						console.log('Error: '+ data);
+					});
+		}
+		
+	};
+
+    $scope.changeTypeFilter = function(typeFilter){
+        $scope.typeFilter = typeFilter;
+    }
+    
+     $scope.changeSearchFilter = function(searchFilter){
+        $scope.searchFilter = searchFilter;
+    }
+    
+	$scope.getAll = function(){
+		$scope.entries = $scope.todos;
+	};
+
+	$scope.setCompleteChecked = function(){
+		$scope.completeChecked = !$scope.completeChecked;
+	};
+
+	$scope.progressFilter = function(entry){
+		if($scope.completeChecked)
+			return entry.progress < 100;
+		return true;
+	};
+}
---
+++ server.js
@@ -0,0 +1,155 @@
+// set up
+var SERVER_PORT = 8089;
+var MONGO_USER = 'Hyperion';
+var MONGO_PASSWORD = 'darktemplar0';
+
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var uriUtil = require('mongodb-uri');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var MONGO_URI = 'mongodb://'+MONGO_USER+':'+MONGO_PASSWORD+'@ds037195.mongolab.com:37195/kalayaan';
+console.log(MONGO_URI);
+var MONGOOSE_URI = uriUtil.formatMongoose(MONGO_URI);
+console.log(MONGOOSE_URI);
+
+//config
+mongoose.connect(MONGOOSE_URI, function(error){
+  if (error) console.error("error: "+error);
+  else console.log('mongo connected');
+});
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+//define model
+var Todo = mongoose.model('newTodo',{
+  'message' : String,
+  'title': String,
+  'progress': Number,
+  'todo_type': String,
+  'date_started': {type: Date, default: Date.now},
+  'date_finished': Date
+});
+
+//routes
+app.get('/api/todos', function(req,res){
+  Todo.find(function(err, todos){
+    if(err) 
+      res.send(err);
+
+    res.json(todos);
+  });
+
+});
+
+app.post('/api/todos', function(req,res){
+  console.log('Posting data: '+ req.body.title);
+  Todo.create({
+    'message': req.body.message,
+    'title': req.body.title,
+    'progress':0  ,
+    'todo_type': req.body.todo_type,
+    done: false
+  }, function(err, todo){
+      console.log(err);
+      if(err)
+        res.send(err);
+
+      Todo.find(function(err, todos){
+        if(err)
+          res.send(err);
+
+        res.json(todos);
+      });
+  });
+});
+
+app.put('/api/todos/update/:_id', function(req,res){
+  console.log('Updating data: '+ req.body.title);
+  var date_finished = null;
+  if(req.body.progress=="100" && 
+        (req.body.date_finished != null && req.body.date_finished == "")){
+    console.log("Task recorded as finished!: "+req.body.title);
+    date_finished = new Date();
+  }
+  else if((req.body.date_finished != null && req.body.date_finished != "")){
+    date_finished = Date.parse(req.body.date_finished);
+  }
+  Todo.update(
+    {_id:req.body._id},
+      
+    {
+      $set:
+      {
+        'message': req.body.message,
+        'title': req.body.title,
+        'progress': req.body.progress,
+        'date_finished': date_finished
+      }
+    }, 
+    {},
+    function(err, todo){
+      console.log(err);
+      if(err)
+        res.send(err);
+
+      Todo.find(function(err, todos){
+        if(err)
+          res.send(err);
+
+        res.json(todos);
+      });
+  });
+});
+
+
+app.delete('/api/todos/:todo_id', function(req,res){
+  Todo.remove({
+    _id: req.params.todo_id
+  }, function(err, todo){
+      if(err)
+        res.send(err)
+
+      Todo.find(function(err, todos){
+        if(err)
+          res.send(err)
+
+        res.json(todos);
+      });
+  });
+});
+
+app.put('/api/todos/delete/multiple', function(req,res){
+  console.log(req);
+  Todo.remove({
+    _id: {$in : req.body._ids}
+  }, function(err, todo){
+    console.log("delete logs:")
+      console.log(err);
+      
+      if(err)
+        res.send(err)
+
+      Todo.find(function(err, todos){
+        if(err)
+          res.send(err)
+        res.json(todos);
+      });
+  });
+});
+
+app.get('*', function(req, res){
+  res.sendfile('./public/core.html');
+});
+
+
+// listen on server port; start server
+app.listen(SERVER_PORT);
+console.log("App listening on port "+ SERVER_PORT);
